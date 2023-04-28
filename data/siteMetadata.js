c907b16758841c3413e68f7f8b9fcba0d80fee3c
dedi-alnas86 <78477428+dedi-alnas86@users.noreply.github.com>
2023-04-28 09:50:31 +0700

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'Dedi Alnas Blog Dengan NEXT.JS',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://dedialnas-nextjs.netlify.app/',
+  siteRepo: 'https://github.com/dedi-alnas86/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
