a611839a4c7c88a5a04beea5faea9547975e02ca
dedi-alnas86 <78477428+dedi-alnas86@users.noreply.github.com>
2023-04-28 09:58:51 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -22,8 +22,8 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+          <Link href="https://github.com/dedi-alnas86/tailwind-nextjs-starter-blog">
+            Dedi Alnas Tailwind Nextjs Theme
           </Link>
         </div>
       </div>
